50e5bba2e1b81964ceabc3f9d3ce640667908aeb
chanvi <hadrien.chanvillard-sabatte@etu.univ-nantes.fr>
2025-12-09 17:34:41 +0100

    game_history

--- pages/Historique.py
+++ pages/Historique.py
@@ -1,17 +1,33 @@
 import streamlit as st
 from src.game import CricketGame
+from utils import get_client
+from datetime import datetime
 
 
-game = CricketGame(game_id=9)
-
-state_df, points_df = game.get_df_to_print()
-
-st.dataframe(
-    state_df,
-    column_config={
-        name: st.column_config.ImageColumn(name)
-        for name in game.player_list
-    },
+client = get_client()
+games = (
+    client
+    .table("games")
+    .select("id", "created_at")
+    .eq("is_finished", True)
+    .order("created_at", desc=True)
+    .execute()
 )
 
-st.dataframe(points_df, hide_index=True)
+for game_info in games.data:
+
+    game_id = game_info["id"]
+    date = datetime.fromisoformat(game_info['created_at'])
+
+    st.write(f"Date: {date.strftime('%d/%m/%Y %H:%M')}")
+
+    game = CricketGame(game_id=game_id)
+    state_df, points_df = game.get_df_to_print()
+    st.dataframe(
+        state_df,
+        column_config={
+            name: st.column_config.ImageColumn(name)
+            for name in game.player_list
+        },
+    )
+    st.dataframe(points_df, hide_index=True)
