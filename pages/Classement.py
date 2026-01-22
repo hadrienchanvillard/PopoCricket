d415f55ea71209a193c31ed0792b4e636340b9f7
chanvi <hadrien.chanvillard-sabatte@etu.univ-nantes.fr>
2026-01-22 13:54:39 +0100

    fix bug when no player in db

--- pages/Classement.py
+++ pages/Classement.py
@@ -8,12 +8,14 @@ st.set_page_config(
 )
 
 leaderboard_data = get_leaderbord()
+print(leaderboard_data)
 index_leaderboard = (["🥇", "🥈", "🥉"] + [str(i) for i in range(4, len(leaderboard_data)+1)])[:len(leaderboard_data)]
 
 leaderboard = pd.DataFrame(
     leaderboard_data,
     index=index_leaderboard,
-    columns=["Joueur","Score"]
 )
+if not leaderboard.empty:
+    leaderboard.columns = ["Joueur","Score"]
 
 st.dataframe(leaderboard)
